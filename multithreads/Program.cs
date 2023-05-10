6f2971f1d8d1e77c984ada99835ddd9614958521
giammatteoenzo <giammatteoenzo@gmail.com>
2023-05-10 14:55:25 +0200

    use case where multi threading is slowler

---
+++ multithreads/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+
+string fileNameBase = "file_";
+string fileExtension = ".txt";
+int numFiles = 10;
+int numChars = 100000;
+
+void deleteAllFiles()
+{
+    for (int i = 0; i < numFiles; i++)
+    {
+        string filePath = "./files/" +  i.ToString() + fileExtension;
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+}
+
+deleteAllFiles();
+
+Random rand = new Random();
+Stopwatch stopwatch = new Stopwatch();
+
+Console.WriteLine("Création de " + numFiles + " fichiers de " + numChars + " caractères sans paralélisation ...");
+
+stopwatch.Start();
+
+for (int i = 0; i < numFiles; i++)
+{
+    string filePath = "./files/" +  i.ToString() + fileExtension;
+    
+    string randomText = "";   
+    for (int j = 0; j < numChars; j++)
+    {
+        randomText += (char)rand.Next(32, 127);
+    }
+
+    using (StreamWriter sw = File.CreateText(filePath))
+    {
+        sw.WriteLine(randomText);
+    }
+}
+
+stopwatch.Stop();
+Console.WriteLine("Création des fichiers sans paralélisation  terminée en " + stopwatch.ElapsedMilliseconds + " ms.");
+
+deleteAllFiles();
+
+Console.WriteLine("Création de " + numFiles + " fichiers de " + numChars + " caractères avec paralélisation ...");
+stopwatch.Restart();
+Parallel.For(0, numFiles, i =>
+{
+    string filePath = "./files/" + i.ToString() + fileExtension;
+    string randomText = "";
+    
+    for (int j = 0; j < numChars; j++)
+    {
+        randomText += (char) rand.Next(32, 127);
+    }
+
+    using (StreamWriter sw = File.CreateText(filePath))
+    {
+        sw.WriteLine(randomText);
+    }
+});
+stopwatch.Stop();
+Console.WriteLine("Création des fichiers avec paralélisation  terminée en " + stopwatch.ElapsedMilliseconds + " ms.");
+
+Console.ReadLine();
